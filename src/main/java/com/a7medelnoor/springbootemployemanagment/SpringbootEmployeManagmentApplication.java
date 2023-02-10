a74a49ee50a360a99642d1418a4a8579eae96fed
ahmed <acc.ahmedstudio@gmail.com>
2023-02-10 22:30:17 +0400

    Issues with hibernate and jdbc driver mysql connection were fixed

--- src/main/java/com/a7medelnoor/springbootemployemanagment/SpringbootEmployeManagmentApplication.java
+++ src/main/java/com/a7medelnoor/springbootemployemanagment/SpringbootEmployeManagmentApplication.java
@@ -2,13 +2,21 @@ package com.a7medelnoor.springbootemployemanagment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.a7medelnoor.springbootemployemanagment.model"})  // force scan JPA entities
 public class SpringbootEmployeManagmentApplication {
+	private static ConfigurableApplicationContext applicationContext;
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootEmployeManagmentApplication.class, args);
+		SpringbootEmployeManagmentApplication.applicationContext = SpringApplication.run(SpringbootEmployeManagmentApplication.class, args);
 	}
 
 }
